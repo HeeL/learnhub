1b490ec726d488658bd5fd072730c000fb5341ac
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2019-01-31 23:52:00 +0100

    Apply some stylings using emotion

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default () => <div>Learnhub</div>
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+
+const headlineStyles = css`
+    color: #2E8B57;
+`;
+const Headline = styled.h1`
+  ${headlineStyles};
+`;
+
+export default () => <Headline>Learnhub</Headline>
