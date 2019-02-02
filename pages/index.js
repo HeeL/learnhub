278986184cdfbfed7b8ab5c3a6600e179222aeec
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2019-02-02 13:14:15 +0100

    Style headline with flexbox

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,20 @@ import React from 'react';
 import Head from '../components/Head';
 
 const headlineStyles = css`
-    color: #2E8B57;
-    font-size: 4rem;
-    font-weight: bold;
-    letter-spacing: 0.1rem;
+  display: flex;
+  justify-content: center;
+  color: #2E8B57;
+  font-size: 4rem;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
 `;
 const SubHeadlineStyles = css`
-    color: #cccccc;
-    font-size: 1.2rem;
-    font-style: italic;
-    letter-spacing: 0.2rem;
-    margin-left: 2rem;
+  display: flex;
+  justify-content: center;
+  color: #cccccc;
+  font-size: 1.2rem;
+  font-style: italic;
+  letter-spacing: 0.2rem;
 `;
 const Headline = styled.div`
   ${headlineStyles};
@@ -22,15 +25,11 @@ const Headline = styled.div`
 const SubHeadline = styled.span`
   ${SubHeadlineStyles};
 `;
-const Container = styled.div`
-`;
 
 export default () => (
   <React.Fragment>
     <Head title="Learnhub - learn where to learn" />
-    <Container>
-      <Headline>Learnhub</Headline>
-      <SubHeadline>Learn where to learn</SubHeadline>
-    </Container>
+    <Headline>Learnhub</Headline>
+    <SubHeadline>Learn where to learn</SubHeadline>
   </React.Fragment>
 );
